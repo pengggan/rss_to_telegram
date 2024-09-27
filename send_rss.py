c2644e4ac87d204c8dbe27dc611b2a1630d31fc2
好酒 <106035465+penggan00@users.noreply.github.com>
2024-09-27 13:30:39 +0800

    Update send_rss.py

--- send_rss.py
+++ send_rss.py
@@ -8,6 +8,20 @@ import json
 # 直接在代码中配置RSS URL列表
 RSS_URLS = [
     "https://rsshub.app/zaobao/znews/china",
+    "https://rsshub.app/guancha/headline",
+    "https://rsshub.app/youtube/playlist/PLRQMDFCUMjJW_R29PyDKbILE2Nj6mC3X3",
+    "https://blog.090227.xyz/atom.xml",
+    "https://rsshub.app/youtube/playlist/PLvrTMNP6Iw6oTPlmRHvjAWiCeQpwHK6yG",
+    "https://www.freedidi.com/feed",
+    "https://www.digihubs.xyz/feeds/posts/default?alt=rss",
+    "https://p3terx.com/feed",
+    "https://www.youtube.com/@bulianglin/videos",
+    "https://www.youtube.com/@IamJackLiu/videos",
+    "https://rsshub.app/fortunechina",
+    "https://www.youtube.com/@%E4%B8%AD%E6%8C%87%E9%80%9A/videos",
+    "https://www.youtube.com/@TchLiyongle/videos",
+    "https://sspai.com/feed",
+    "https://www.112114.xyz/rss",
     "https://hunsh.net/atom.xml"
 ]
 
